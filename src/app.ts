9c90f70f4e636b0504f5e2a312c8c37704058817
mpnri <mpnri2831@gmail.com>
2022-06-29 19:08:28 +0430

    Complete Theme Changer + Add  * typescript & parcel  * effects for theme icon  * :root -> light & dark & default theme styles

---
+++ src/app.ts
@@ -0,0 +1,46 @@
+import sun from '../assets/images/icon-sun.svg';
+import moon from '../assets/images/icon-moon.svg';
+
+enum THEMES {
+    LIGHT = 'LIGHT',
+    DARK = 'DARK',
+}
+
+const styles = {
+    [THEMES.DARK]: {
+        '--body-background': 'var(--very-dark-desaturated-blue)',
+        '--body-color': 'var(--light-grayish-blue)',
+        '--bg-header-small': 'var(--dark-bg-image-small)',
+        '--bg-header-large': 'var(--dark-bg-image-large)',
+        '--todo-card-bg-color': 'var(--very-dark-grayish-blue-hover)',
+        '--todo-card__close-filter': 'var(--dark-close-filter)',
+        '--todo-card-border-color': 'var(--very-dark-grayish-blue)',
+    },
+    [THEMES.LIGHT]: {
+        '--body-background': 'var(--very-light-gray)',
+        '--body-color': 'var(--very-dark-grayish-blue)',
+        '--bg-header-small': 'var(--light-bg-image-small)',
+        '--bg-header-large': 'var(--light-bg-image-large)',
+        '--todo-card-bg-color': 'var(--very-light-gray)',
+        '--todo-card__close-filter': 'var(--light-close-filter)',
+        '--todo-card-border-color': 'var(--very-light-grayish-blue)',
+    },
+}
+
+let theme = THEMES.LIGHT;
+
+document.querySelector('.header__icon')?.addEventListener('click', e => {
+    const target = (e.currentTarget as HTMLDivElement).children[0] as HTMLImageElement;
+    theme = theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
+    
+    target.src = theme === THEMES.LIGHT ? moon : sun;
+    renderTheme();
+})
+
+
+renderTheme();
+
+function renderTheme() {
+    Object.keys(styles[theme]).forEach(key => document.documentElement.style.setProperty(key, styles[theme][key]));
+    
+}
